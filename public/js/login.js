3b84ecfe89e50c81af8974ec6bd102aaec8cec89
unknown <eastflow815@gmail.com>
2021-12-13 14:55:07 +0900

    #2 누락기능 추가

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 const $loginBtn = document.querySelector('.login');
 const $loginWrap = document.querySelector('.login-wrap');
-// const $signinBtn = document.querySelector('#signin-btn');
+const $signinBtn = document.querySelector('#signin-btn');
 // const $form = document.querySelector('#form');
 
 
@@ -46,8 +46,8 @@ $loginWrap.addEventListener('click', e => {
     <div class="login-box">
     <div class="login-banner">
       <!-- 로고 대체 이미지 아무거나 집어넣음 -->
-      <img src="img/favicon-32x32.png" alt="">
-      <span class="material-icons">close</span>
+      <img src="./img/favicon-32x32.png" alt="">
+      <span class="material-icons close-icon">close</span>
     </div>
     <div class="login-body">
       <form id="form">
@@ -59,60 +59,61 @@ $loginWrap.addEventListener('click', e => {
           <span>PW</span>
            <input type="password" class="input" placeholder="비밀번호를 입력하세요">
         </label>
-        <button type="submit" id="signup-btn">sign up</button>
+        <button type="submit" id="signin-btn">sign up</button>
       </form>
-      <a href="javascript:void(0)" id="signin">로그인</a>
-    </div>
-  </div>`
+      <a href="javascript:void(0)" id="signin">로그인</a>`
   }
 
   if(e.target === document.querySelector('#signin')) {
     $loginWrap.innerHTML = `
     <div class="login-box">
-    <div class="login-banner">
-      <!-- 로고 대체 이미지 아무거나 집어넣음 -->
-      <img src="img/favicon-32x32.png" alt="">
-      <span class="material-icons">close</span>
-    </div>
-    <div class="login-body">
-      <form id="form">
-        <label for="" class="input-label">
-          <span>ID</span>
-          <input type="text" class="input" placeholder="아이디를 입력하세요">
-        </label>
-        <label for="" class="input-label">
-          <span>PW</span>
-           <input type="password" class="input" placeholder="비밀번호를 입력하세요">
-        </label>
-        <button type="submit" id="signin-btn">sign in</button>
-      </form>
-      <a href="javascript:void(0)" id="signup">회원가입</a>
-    </div>
-  </div>
+      <div class="login-banner">
+        <!-- 로고 대체 이미지 아무거나 집어넣음 -->
+        <img src="./img/favicon-32x32.png" alt="">
+        <span class="material-icons close-icon">close</span>
+      </div>
+      <div class="login-body">
+        <form id="form">
+          <label for="" class="input-label">
+            <span>ID</span>
+            <input type="text" class="input" placeholder="아이디를 입력하세요">
+          </label>
+          <label for="" class="input-label">
+            <span>PW</span>
+             <input type="password" class="input" placeholder="비밀번호를 입력하세요">
+          </label>
+          <button type="submit" id="signin-btn">sign in</button>
+        </form>
+        <a href="javascript:void(0)" id="signup">회원가입</a>
     `
+  }
+
+  if(e.target === document.querySelector('.close-icon')){
+    $loginWrap.style.display = "none";
   }
 });
 
 
 // 로그인 버튼 (현재 post데이터 통신을 위해 테스트중)
-// $signinBtn.addEventListener('click', e => {
-//   e.preventDefault();
-//   // console.log($form[0].value);
-//   // console.log($form[1].value);
+// 기본동작 막아둠, 회원가입의 로그인 버튼은 막지 못함
+$signinBtn.addEventListener('click', e => {
+  e.preventDefault();
+  // console.log($form[0].value);
+  // console.log($form[1].value);
 
-//   request.post('/todos', {
-//     id: 1,
-//     name: $form[0].value,
-//     pw: $form[1].value
-//   }).then(res => {
-//     if(!res.ok) throw new Error(res.statusText);
-//     return res.json();
-//   })
-//   .then(todos => console.log(todos))
-//   .catch(err => console.log(err));
+  // request.post('/todos', {
+  //   id: 1,
+  //   name: $form[0].value,
+  //   pw: $form[1].value
+  // }).then(res => {
+  //   if(!res.ok) throw new Error(res.statusText);
+  //   return res.json();
+  // })
+  // .then(todos => console.log(todos))
+  // .catch(err => console.log(err));
 
-//   // request.get('/todos')
-//   // .then(res => res.json())
-//   // .then(todos => console.log(todos))
-//   // .catch(err => console.log(err));
-// })
+  // request.get('/todos')
+  // .then(res => res.json())
+  // .then(todos => console.log(todos))
+  // .catch(err => console.log(err));
+});
