18dc89ea27fef2758844ce73a04f26b8eca91615
ysh2987 <ysh2987@naver.com>
2021-12-14 21:40:15 +0900

    Refactor: 버그 수정 및 리펙토링

--- public/writing/writing.js
+++ public/writing/writing.js
@@ -1,3 +1,6 @@
+import { todayFormat } from '../utils/date.js';
+import { FILTER_KEYWORDS } from '../Home/filter.js';
+
 const $title = document.querySelector('.title');
 const $city = document.querySelector('.city');
 const $sports = document.querySelector('.sports');
@@ -8,41 +11,47 @@ const $sportsItems = document.querySelector('.sports-items');
 const $writeArea = document.querySelector('.writing-area');
 
 // post
-const request = {
-  post(url, payload) {
-    return fetch(url, {
+const postingSend = async payload => {
+  try {
+    const res = await fetch('/api/postings', {
       method: 'POST',
       headers: { 'content-Type': 'application/json' },
       body: JSON.stringify(payload),
     });
-  },
+    if (!res.ok) throw new Error(res.status);
+    window.location.href = '../../index.html';
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const postingSend = payload => {
-  request
-    .post('/api/postings', payload)
-    .then(resposne => resposne.json())
-    .then((window.location.href = '../../index.html'))
-    .catch(err => console.error(err));
+// 지역, 스포츠 리스트 생성
+const listCreate = (categoriesArr, categories) => {
+  const $fragment = document.createDocumentFragment();
+  [...categoriesArr].forEach((category, index) => {
+    const $li = document.createElement('li');
+    $li.dataset.index = index;
+    $li.textContent = category;
+    $fragment.append($li);
+  });
+  categories.append($fragment);
 };
 
-const todayFormat = () => {
-  const today = new Date();
-  const format = num => (num < 10 ? `0${num}` : `${num}`);
-  return `${today.getFullYear()}/${format(today.getMonth() + 1)}/${format(today.getDate())}`;
-};
+listCreate(FILTER_KEYWORDS.CITY, $cityList);
+listCreate(FILTER_KEYWORDS.SPORTS, $sportsList);
 
 document.querySelector('.writing-submit').addEventListener('click', () => {
+  const $cityItem = document.querySelector('.city-item > span');
   // 임시 처리
   if ($title.value.trim() === '') {
     alert('제목을 입력해주세요');
     return;
   }
-  if ($cityItems.childNodes.length === 0) {
+  if (!$cityItem) {
     alert('지역을 선택해주세요');
     return;
   }
-  if ($sportsItems.childNodes.length === 0) {
+  if ($sportsItems.children.length === 0) {
     alert('스포츠를 선택해주세요');
     return;
   }
@@ -50,49 +59,70 @@ document.querySelector('.writing-submit').addEventListener('click', () => {
     alert('본문을 입력해주세요');
     return;
   }
-  const cityArr = [...$cityItems.querySelectorAll('span')].map(city => city.getAttribute('data-index'));
+  // const cityArr = [...$cityItems.querySelectorAll('span')].map(city => city.getAttribute('data-index'));
   const sportsArr = [...$sportsItems.querySelectorAll('span')].map(sports => sports.getAttribute('data-index'));
   const writeVaules = {
     title: $title.value,
-    city: cityArr,
+    city: $cityItem.textContent,
     sportsType: sportsArr,
     content: $writeArea.value,
     date: todayFormat(),
   };
+  console.log(writeVaules);
   postingSend(writeVaules);
 });
 
 // 중복 방지
-const duplication = (items, e) => {
+const duplication = (items, selectItem) => {
   let check = false;
   [...items.querySelectorAll('span')].forEach(item => {
-    if (item.textContent === e.target.textContent) check = true;
+    if (item.textContent === selectItem) check = true;
   });
   return check;
 };
 
-// city 선택
+// city 단일 선택
 $cityList.addEventListener('click', e => {
   if (!e.target.matches('li')) return;
-  if (duplication($cityItems, e)) return;
-  $cityItems.innerHTML += `
-	<div class="city-item">
+  // if (duplication($cityItems, e.target.textContent)) return;
+  $cityItems.innerHTML = `
+	<li class="city-item">
 		<span data-index="${e.target.getAttribute('data-index')}">${e.target.textContent}</span>
-		<i class="bx bx-x one-delete"></i>
-	</div>
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    class="one-delete"
+    >
+    <path
+      d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"
+    ></path>
+  </svg>
+	</li>
 	`;
   $city.classList.toggle('active');
 });
 
-// sports 선택
+// sports 복수 선택
 $sportsList.addEventListener('click', e => {
   if (!e.target.matches('li')) return;
-  if (duplication($sportsItems, e)) return;
+  if (duplication($sportsItems, e.target.textContent)) return;
   $sportsItems.innerHTML += `
-	<div class="sports-item">
+	<li class="sports-item">
 		<span data-index="${e.target.getAttribute('data-index')}">${e.target.textContent}</span>
-		<i class="bx bx-x one-delete"></i>
-	</div>
+		<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    class="one-delete"
+    >
+    <path
+      d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"
+    ></path>
+  </svg>
+	</li>
 	`;
   $sports.classList.toggle('active');
 });
