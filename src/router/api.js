83d60d9d4c28a9d1978e2d458dbebc3977cbffed
highcastlee <highcastlee@naver.com>
2021-12-15 15:42:01 +0900

    Refactor: 게시글 변수명 posting -> post 수정

--- src/router/api.js
+++ src/router/api.js
@@ -22,7 +22,7 @@ const FILTER = {
 };
 
 // Mock Data
-let postings = [
+let posts = [
   {
     id: 0,
     title: `title ${0}`,
@@ -110,37 +110,37 @@ let postings = [
 const apiRouter = express.Router();
 
 // GET
-apiRouter.get('/postings', (req, res) => {
-  let sendingData = [...postings];
+apiRouter.get('/posts', (req, res) => {
+  let sendingData = [...posts];
   if (req.query.cities && req.query.sports) {
     const currentCities = req.query.cities.split(',');
     const currentSports = req.query.sports.split(',');
     sendingData = sendingData.filter(
-      posting =>
-        currentCities.includes(FILTER.CITIES[posting.city]) &&
-        posting.sportsType.some(sports => currentSports.includes(FILTER.SPORTS[sports])),
+      post =>
+        currentCities.includes(FILTER.CITIES[post.city]) &&
+        post.sportsType.some(sports => currentSports.includes(FILTER.SPORTS[sports])),
     );
   }
   res.status(200).json(sendingData);
 });
 
-apiRouter.get('/postings/:id', (req, res) => {
+apiRouter.get('/posts/:id', (req, res) => {
   const { id } = req.params;
 
-  const posting = postings.filter(posting => posting.id === +id);
+  const post = posts.filter(post => post.id === +id);
 
-  res.send(posting);
+  res.send(post);
 });
 
 // POST
-apiRouter.post('/postings', (req, res) => {
+apiRouter.post('/posts', (req, res) => {
   // body is not null
   const { title, city, sportsType, content, date } = req.body;
 
-  const maxId = (() => Math.max(...postings.map(({ id }) => id)))();
+  const maxId = (() => Math.max(...posts.map(({ id }) => id)))();
 
   try {
-    const newPosting = {
+    const newPost = {
       id: maxId + 1,
       title,
       writer: 'writer 1',
@@ -151,16 +151,16 @@ apiRouter.post('/postings', (req, res) => {
       recruit: true,
     };
 
-    postings = [...postings, newPosting];
+    posts = [...posts, newPost];
 
-    res.status(200).send(postings);
+    res.status(200).send(posts);
   } catch (error) {
     res.status(400).send(error);
   }
 });
 
 // PATCH
-apiRouter.patch('/postings/:id', (req, res) => {
+apiRouter.patch('/posts/:id', (req, res) => {
   const {
     params: { id },
     // body is not null
@@ -168,23 +168,23 @@ apiRouter.patch('/postings/:id', (req, res) => {
   } = req;
 
   try {
-    postings = postings.map(post =>
+    posts = posts.map(post =>
       post.id === +id ? Object.assign(post, { title, city, sportsType, content, recruit }) : post,
     );
 
-    res.send(postings);
+    res.send(posts);
   } catch (error) {
     res.status(400).send(error);
   }
 });
 
 // DELETE
-apiRouter.delete('/postings/:id', (req, res) => {
+apiRouter.delete('/posts/:id', (req, res) => {
   const { id } = req.params;
 
-  postings = postings.filter(post => post.id !== +id);
+  posts = posts.filter(post => post.id !== +id);
 
-  res.send(postings);
+  res.send(posts);
 });
 
 export default apiRouter;
