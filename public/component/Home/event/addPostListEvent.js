0c4759bb76c5c734fbacf15e8fbacf7874edbdd6
highcastlee <highcastlee@naver.com>
2021-12-21 20:46:57 +0900

    Refactor: Component 생성 시, element 교체

--- public/component/Home/event/addPostListEvent.js
+++ public/component/Home/event/addPostListEvent.js
@@ -1,4 +1,4 @@
-import Comment from '../../Post/Comment.js';
+import { CommentComponent } from '../../Post/Comment.js';
 
 export default function addPostListEvent ($parent, $target) {
   const $postList = $target.querySelector('.post-list');
@@ -6,8 +6,7 @@ export default function addPostListEvent ($parent, $target) {
     const $li = e.target.closest('.post');
     if (!$li || $li.classList.contains('opacity')) return;
     const { id } = $li.dataset;
-    window.history.pushState({}, `/posts:${id}`, window.location.origin + `/posts:${id}`);
-    $parent.removeChild($parent.lastChild);
-    new Comment({ $parent, initialState: {} });
+    window.history.pushState({}, `/posts?id=${id}`, window.location.origin + `/posts?id=${id}`);
+    CommentComponent($parent);
   });
 }
