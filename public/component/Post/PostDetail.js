51189731b109b8521d79e012caea6dede2a063b7
chmini <beelzebule@naver.com>
2021-12-22 17:15:32 +0900

    Refactor: delete rewind svg

--- public/component/Post/PostDetail.js
+++ public/component/Post/PostDetail.js
@@ -1,6 +1,6 @@
 import addPostDetailEvent from './event/addPostDetailEvent.js';
 
-export default function PostDetail ({ $parent, initialState }) {
+export default function PostDetail({ $parent, initialState }) {
   this.state = initialState;
   this.$target = document.createElement('main');
   if ($parent.children.length > 1) $parent.removeChild($parent.lastChild);
@@ -26,24 +26,6 @@ export default function PostDetail ({ $parent, initialState }) {
         </div>
       </div>
       <div class="post-container">
-        <div class="rewind">
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            stroke-width="0"
-            viewBox="0 0 448 512"
-            color="808080"
-            cursor="pointer"
-            height="30"
-            width="30"
-            xmlns="http://www.w3.org/2000/svg"
-            style="color: rgb(128, 128, 128)"
-          >
-            <path
-              d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"
-            ></path>
-          </svg>
-        </div>
         <article class="post-box box" data-id="1"></article>
         <article class="comment-box box"></article>
       </div>`;
